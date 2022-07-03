e7c147b5f9ad4eef4b504cb85b53a9d1bca505e8
KurtDev599 <sw536880@naver.com>
2022-07-03 21:29:03 +0900

    middle ware 만들기

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.use((req, res, next) => {
+  console.log('all router')
+  next()
+})
+
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
@@ -10,6 +15,14 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.send('hello express')
+})
+
+app.get('/category/:name', (req, res) => {
+  res.send(`hello ${req.params.name}`)
+})
+
+app.get('*', (req, res) => {
+  res.send('all request')
 })
 
 app.post('/', (req, res) => {
